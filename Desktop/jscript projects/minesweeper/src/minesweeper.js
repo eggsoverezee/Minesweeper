db49308847bd9c096fb9b956ba255a1b249a0766
EBraxton <braxton.emmanuel@gmail.com>
2018-03-26 20:12:45 -0700

    Same 3x3 game board.. board automatically formatted

---
+++ Desktop/jscript projects/minesweeper/src/minesweeper.js
@@ -0,0 +1,15 @@
+const printboard = board => {
+  console.log("Current Board:");
+  console.log(board[0].join(' | '));
+  console.log(board[1].join(' | '));
+  console.log(board[2].join(' | '));
+};
+let board = [
+  [' ', ' ', ' '],
+  [' ', ' ', ' '],
+  [' ', ' ', ' ']
+];
+printboard(board);
+board[0][1] = '1';
+board[2][2] = 'B';
+printboard(board);
